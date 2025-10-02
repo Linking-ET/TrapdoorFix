7e517f43a2e3a1245d081694f0aa550c7135aa92
XChen446 <3088576338@qq.com>
2025-10-03 03:39:22 +0800

    更新版本号

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "let.xiaochen"
-version = "1.0-SNAPSHOT"
+version = "2.0-SNAPSHOT"
 
 repositories {
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
